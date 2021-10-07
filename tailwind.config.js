081108091f8d986bdddf02c313a1745f94fb8512
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-10-07 21:11:26 +0900

    style: login page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      opacity: ["group-hover"],
+    },
+    colors: {
+      sky: colors.sky,
+      blue: colors.blue,
+      amber: colors.amber,
+      white: colors.white,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
