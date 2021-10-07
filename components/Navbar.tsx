7826e0fa36d4a1bb3076de478e6a2cfb40906da9
강명구 <mg5566@hanmail.net>
2021-10-07 17:56:10 +0900

    chore: nav bar 에 search input, dropdown 추가, 페이지 전환 test 완료

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,30 @@
+import Link from "next/link";
 import React, { VFC } from "react";
 import Dropdown from "./Dropdown";
+import SearchInput from "./SearchInput";
 
 const Navbar: VFC = () => {
   return (
-    <div className="flex flex-row h-auto bg-blue-100">
-      <div className="flex-grow w-16 justify-start">image</div>
-      <div className="flex flex-row justify-center">
-        <div className="w-16">list 1</div>
-        <div className="w-16">list 2</div>
-        <div className="w-16">list 3</div>
-        <div className="w-16">list 4</div>
+    <div className="flex flex-row h-auto bg-blue-100 justify-between">
+      <div className="w-auto">Logo image</div>
+      <div className="flex flex-row">
+        <div className="w-16">
+          <Link href="/TestSamplePage">list 1</Link>
+        </div>
+        <div className="w-16">
+          <Link href="/TestSamplePage">list 2</Link>
+        </div>
+        <div className="w-16">
+          <Link href="/TestSamplePage">list 3</Link>
+        </div>
+        <div className="w-16">
+          <Link href="/TestSamplePage">list 4</Link>
+        </div>
       </div>
-      <div className="flex flex-grow justify-end">
+      <div className="focus:border-indigo-500">
+        <SearchInput></SearchInput>
+      </div>
+      <div className="flex">
         <div className="w-16">ID</div>
         <Dropdown></Dropdown>
       </div>
