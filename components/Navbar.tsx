dbb05edaacbaf3d39bd12c62abe3afb51dfa00e8
강명구 <mg5566@hanmail.net>
2021-10-07 17:26:00 +0900

    feat: navigation bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,22 @@
 import React, { VFC } from "react";
+import Dropdown from "./Dropdown";
 
 const Navbar: VFC = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex flex-row h-auto bg-blue-100">
+      <div className="flex-grow w-16 justify-start">image</div>
+      <div className="flex flex-row justify-center">
+        <div className="w-16">list 1</div>
+        <div className="w-16">list 2</div>
+        <div className="w-16">list 3</div>
+        <div className="w-16">list 4</div>
+      </div>
+      <div className="flex flex-grow justify-end">
+        <div className="w-16">ID</div>
+        <Dropdown></Dropdown>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
