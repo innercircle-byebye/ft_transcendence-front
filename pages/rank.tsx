533b42aacc718fa11fad6e40db7646fe3ffdfc11
paikwiki <paikwiki@gmail.com>
2021-10-08 18:18:07 +0900

    feat: Rank 페이지 생성

---
+++ pages/rank.tsx
@@ -0,0 +1,13 @@
+import { ReactElement } from "react";
+import { NextPage } from "next";
+import MainLayout from "@/layouts/MainLayout";
+
+const Rank: NextPage = () => {
+  return (
+    <MainLayout>
+      <h1>Rank</h1>
+    </MainLayout>
+  );
+};
+
+export default Rank;
