f851efa7ef50ddc78752cbedefce118ed67a9ceb
paikwiki <paikwiki@gmail.com>
2021-10-08 18:34:39 +0900

    refactor: Rank 페이지에 Rank.getLayout()으로 <MainLayout> 적용

--- pages/rank.tsx
+++ pages/rank.tsx
@@ -2,12 +2,12 @@ import { ReactElement } from "react";
 import { NextPage } from "next";
 import MainLayout from "@/layouts/MainLayout";
 
-const Rank: NextPage = () => {
-  return (
-    <MainLayout>
-      <h1>Rank</h1>
-    </MainLayout>
-  );
+const Rank = () => {
+  return <h1>Rank</h1>;
+};
+
+Rank.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
 };
 
 export default Rank;
