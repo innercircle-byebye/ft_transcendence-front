9c0d3e6af03232b9cbc7284f40b8752ac49722a4
강명구 <mg5566@hanmail.net>
2021-10-09 17:17:11 +0900

    chore: nav bar design 수정

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,37 +5,38 @@ import SearchInput from "./SearchInput";
 
 const Navbar: VFC = () => {
   return (
-    // navbar 높이는 고정 크기를 갖도록
-    <div className="flex flex-row px-3 py-3 h-12 bg-blue-100 justify-between">
-      <div className="">Logo image</div>
-      <div className="space-x-6 flex flex-row">
-        <div className="w-16">
+    <div className="flex flex-row flex-wrap px-12 py-3 bg-blue-100">
+      <div className="px-6 justify-start">Logo image</div>
+      <div className="px-12 mx-12 flex flex-row">
+        <div className="mx-6">
           <Link href="/">
             <a>HOME</a>
           </Link>
         </div>
-        <div className="w-16">
+        <div className="mx-6">
           <Link href="/">
             <a>PLAY</a>
           </Link>
         </div>
-        <div className="w-16">
+        <div className="mx-6">
           <Link href="/">
             <a>RANK</a>
           </Link>
         </div>
-        <div className="w-16">
+        <div className="mx-6">
           <Link href="/">
             <a>CHAT</a>
           </Link>
         </div>
       </div>
-      <div className="">
-        <SearchInput></SearchInput>
+      <div className="px-12 mx-12">
+        <div className="justify-center">
+        <SearchInput />
+        </div>
       </div>
-      <div className="flex w-1/12 justify-end">
-        <div className="w-16">ID</div>
-        <Dropdown></Dropdown>
+      <div className="flex flex-auto justify-end">
+        <div className="mx-2">ID</div>
+        <Dropdown />
       </div>
     </div>
   );
