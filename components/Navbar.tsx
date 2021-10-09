5e19f75eaf6369a55b47ee132262f01b3ce2dcd9
강명구 <mg5566@hanmail.net>
2021-10-09 17:47:55 +0900

    temporary commit : type 관련 문제 발생

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,37 @@ import React, { VFC } from "react";
 import Dropdown from "./Dropdown";
 import SearchInput from "./SearchInput";
 
+const navigationList = [
+  { name: "HOME", href: "/", current: true },
+  { name: "PLAY", href: "/", current: false },
+  { name: "RANK", href: "/", current: false },
+  { name: "CHAT", href: "/", current: false },
+];
+
+const NavList = (props) => {
+  const lists = props.list;
+  const listItems = lists.map((item) => (
+    <Link key={item.name} href={item.href}>
+      <a>{item.name}</a>
+    </Link>
+  ));
+};
+
 const Navbar: VFC = () => {
   return (
     <div className="flex flex-row flex-wrap px-12 py-3 bg-blue-100">
       <div className="px-6 justify-start">Logo image</div>
       <div className="px-12 mx-12 flex flex-row">
         <div className="mx-6">
-          <Link href="/">
+          {/* <Link href="/">
             <a>HOME</a>
-          </Link>
+          </Link> */}
+          {/* {navigationList.map((item) => (
+            <Link key={item.name} href={item.href}>
+              <a>{item.name}</a>
+            </Link>
+          ))} */}
+          <NavList list={navigationList} />
         </div>
         <div className="mx-6">
           <Link href="/">
@@ -31,7 +53,7 @@ const Navbar: VFC = () => {
       </div>
       <div className="px-12 mx-12">
         <div className="justify-center">
-        <SearchInput />
+          <SearchInput />
         </div>
       </div>
       <div className="flex flex-auto justify-end">
