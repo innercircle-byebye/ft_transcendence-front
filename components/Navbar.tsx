32f3ee1e96d6c99b0bf66c7c1fc8b2a5dd0ddc58
강명구 <mg5566@hanmail.net>
2021-10-11 20:30:25 +0900

    style: input box layout 수정

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { VFC } from "react";
 import Dropdown from "./Dropdown";
-import SearchInput from "./SearchInput";
+import SearchInputBox from "./SearchInputBox";
 
 interface IListItem {
   name: string;
@@ -44,7 +44,7 @@ const Navbar: VFC = () => {
       {/* user search input box */}
       <div className="px-12">
         <div className="justify-center">
-          <SearchInput />
+          <SearchInputBox />
         </div>
       </div>
       {/* ID 와 Dropdown button */}
--- components/SearchInputBox.tsx
+++ components/SearchInputBox.tsx
@@ -1,19 +1,28 @@
-import { SearchIcon } from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 const SearchInputBox = () => {
+  /*
+  const [User, setUser] = useState("fuck you");
+
+  const onUserHandler = (event: string) => {
+    console.log(User);
+    setUser(event);
+    console.log(User);
+  };
+  */
   return (
-    <div className="mt-1 relative rounded-md shadow-sm">
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <span className="text-gray-500 sm:text-sm">{`>`}</span>
-      </div>
+    <div className="mt-1 relative flex item-center rounded-md shadow-sm h-1/6 w-full">
       <input
         type="text"
-        name="price"
-        id="price"
-        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+        name="UserID"
+        id="UserID"
+        className="focus:ring-gray-500 focus:border-gray-500 block w-full h-1/6 pl-7 pr-12 sm:text-sm border-gray-300 ring-gray-300 rounded-md"
         placeholder="Search user ID"
       />
-      <SearchIcon />
+      <button type="button" className="absolute ml-52 h-full">
+        <SearchIcon className="hover:text-black w-full h-full" />
+      </button>
     </div>
   );
 };
