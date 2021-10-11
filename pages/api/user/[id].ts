1a997e6a3bbfb4b9d1ae5a8354f0522968938509
jiwlee <ddazi9576@gmail.com>
2021-10-11 20:40:08 +0900

    feat: Post 요청에 대한 처리 추가

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,29 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { IUser } from "@/typings/db";
+import { IMessage, IUser } from "@/typings/db";
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<IUser | false>
+  res: NextApiResponse<IUser | false | IMessage>
 ) {
-  res.status(200).json(
-    req.headers.authorization
-      ? {
-        "userId": 1,
-        "email": "marvin@student.42.fr",
-        "intraUsername": "marvin",
-        "nickname": "퐁게임너무재미있네",
-        "imagePath": "string",
-        "status": "not_registered",
-        "experience": 42,
-        "rankId": 1,
-        "banDate": null,
-        "isStatusPublic": true,
-        "isHistoryPublic": true,
-        "createdAt": "2021-10-11T06:28:44.899Z",
-        "lastModifiedAt": "2021-10-11T06:28:44.899Z",
-        "deletedAt": null
-      }
-      : false
-  );
+  if (req.method === "GET") {
+    res.status(200).json(
+      req.headers.authorization
+        ? {
+            userId: 1,
+            email: "marvin@student.42.fr",
+            intraUsername: "marvin",
+            nickname: "퐁게임너무재미있네",
+            imagePath: "",
+            status: "online",
+            experience: 42,
+            rankId: 1,
+            banDate: null,
+            isStatusPublic: true,
+            isHistoryPublic: true,
+            createdAt: "2021-10-11T06:28:44.899Z",
+            lastModifiedAt: "2021-10-11T06:28:44.899Z",
+            deletedAt: null,
+          }
+        : false
+    );
+  } else if (req.method === 'POST') {
+    res.status(200).json({
+      message: "update success"
+    })
+  }
 }
