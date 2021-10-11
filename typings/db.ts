e6e3e21ddfe281c69e358bc48b54d93997be8dfb
jiwlee <ddazi9576@gmail.com>
2021-10-11 15:47:01 +0900

    feat: api 반영해서 데이터 interface 변경, api경로 변경

--- typings/db.ts
+++ typings/db.ts
@@ -1,5 +1,16 @@
 export interface IUser {
-  id: number;
-  nickname: string;
+  userId: number;
   email: string;
+  intraUsername: string;
+  nickname: string;
+  imagePath: string;
+  status: string;
+  experience: number;
+  rankId: number;
+  banDate: string | null;
+  isStatusPublic: boolean;
+  isHistoryPublic: boolean;
+  createdAt: string;
+  lastModifiedAt: string;
+  deletedAt: string | null;
 }
