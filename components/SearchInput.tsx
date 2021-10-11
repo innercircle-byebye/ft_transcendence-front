6371994125a40577783a5adf64b7f1c88928e162
강명구 <mg5566@hanmail.net>
2021-10-11 16:17:10 +0900

    chore: searchInput 파일 제거

--- components/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-export default function SearchInput() {
-  return (
-    <div className="flex flex-row">
-    <input placeholder="search user ID">
-    </input>
-    <div> 돋보기 ICON </div>
-    </div>
-  );
-}
