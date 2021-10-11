90c9c5a2e3c232dc8238016a0a2055419e90a1c7
강명구 <mg5566@hanmail.net>
2021-10-11 21:17:47 +0900

    feat: button 클릭시 onclick event handler 적용

--- components/SearchInputBox.tsx
+++ components/SearchInputBox.tsx
@@ -1,16 +1,19 @@
 import { SearchIcon } from "@heroicons/react/solid";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const SearchInputBox = () => {
-  /*
-  const [User, setUser] = useState("fuck you");
+  const [User, setUser] = useState("");
 
-  const onUserHandler = (event: string) => {
-    console.log(User);
-    setUser(event);
-    console.log(User);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    const UserText = event.target;
+    setUser(UserText.value);
+  }
+
+  const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("User", User);
+    // 향후 State User 를 사용하여 <Link href={`/profile/${User}`} /> 이렇게 하면 될듯?!
   };
-  */
+
   return (
     <div className="mt-1 relative flex item-center rounded-md shadow-sm h-1/6 w-full">
       <input
@@ -19,9 +22,10 @@ const SearchInputBox = () => {
         id="UserID"
         className="focus:ring-gray-500 focus:border-gray-500 block w-full h-1/6 pl-7 pr-12 sm:text-sm border-gray-300 ring-gray-300 rounded-md"
         placeholder="Search user ID"
+        onChange={onChange}
       />
-      <button type="button" className="absolute ml-52 h-full">
-        <SearchIcon className="hover:text-black w-full h-full" />
+      <button type="button" className="absolute ml-52 h-full" onClick={onClickHandler}>
+        <SearchIcon className="hover:text-black w-full h-full"/>
       </button>
     </div>
   );
