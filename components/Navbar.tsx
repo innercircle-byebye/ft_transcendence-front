c233b6e8e22710d8c9f0eb4704d12730244538fd
강명구 <mg5566@hanmail.net>
2021-10-11 15:41:12 +0900

    chore: navbar 에 UserInfoMenu component 추가 및 test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import React, { VFC } from "react";
-import Dropdown from "./Dropdown";
 import SearchInput from "./SearchInput";
+import UserInfoMenu from "./UserInfoMenu";
 
 interface IListItem {
   name: string;
@@ -20,7 +20,7 @@ const Navbar: VFC = () => {
   ];
 
   return (
-    <div className="flex flex-row flex-wrap px-12 py-3 bg-blue-100">
+    <div className="flex flex-row flex-wrap px-12 py-3 bg-blue-100 h-1/12">
       {/* 이미지 */}
       <div className="px-6 justify-start">
         <Image
@@ -50,7 +50,7 @@ const Navbar: VFC = () => {
       {/* ID 와 Dropdown button */}
       <div className="flex flex-auto flex-wrap justify-end">
         <div className="mx-2">ID</div>
-        <Dropdown />
+        <UserInfoMenu />
       </div>
     </div>
   );
