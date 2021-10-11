ee9dd5d5b0c083849a8270645121eb383f1dacf9
jiwlee <ddazi9576@gmail.com>
2021-10-11 15:45:51 +0900

    feat: /create-profile page 임의로 생성

---
+++ pages/create-profile/index.tsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+const CreateProfile = ({
+  userData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+
+  return <div>create-profile</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // if (!context.req.cookies.pong_access_token) {
+  //   return {
+  //     redirect: {
+  //       destination: "/login",
+  //       permanent: false,
+  //     },
+  //   };
+  // }
+
+  // axios.defaults.withCredentials = true;
+  // axios.defaults.headers.common["Authorization"] =
+  //   context.req.cookies.pong_access_token;
+
+  const res = await axios.get("http://localhost:3000/api/user/me");
+
+  return {
+    props: {
+      userData: res.data,
+    },
+  };
+};
+
+export default CreateProfile;
