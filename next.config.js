18f910f5bdb9662b8f97afe81dfdb3da94513c93
jiwlee <ddazi9576@gmail.com>
2021-10-12 19:27:20 +0900

    feat: proxy를 위한 rewrites 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["cdn.intra.42.fr"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://nestjs-back:3005/api/:path*",
+      },
+    ];
+  },
+};
