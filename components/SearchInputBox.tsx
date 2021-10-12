4fb89f863d43ebae9ad7a56df8d70009cb0de1e1
강명구 <mg5566@hanmail.net>
2021-10-12 11:23:17 +0900

    refator: State변수명 변경, input tag name property 를 nickname 으로 변경

--- components/SearchInputBox.tsx
+++ components/SearchInputBox.tsx
@@ -2,15 +2,15 @@ import { SearchIcon } from "@heroicons/react/solid";
 import React, { useState } from "react";
 
 const SearchUserInputBox = () => {
-  const [User, setUser] = useState("");
+  const [user, setUser] = useState("");
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-    const UserText = event.target;
-    setUser(UserText.value);
+    const userText = event.target;
+    setUser(userText.value);
   }
 
   const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("User", User);
+    console.log("User", user);
     // 향후 State User 를 사용하여 <Link href={`/profile/${User}`} /> 이렇게 하면 될듯?!
   };
 
@@ -18,11 +18,11 @@ const SearchUserInputBox = () => {
     <div className="mt-1 relative flex item-center rounded-md shadow-sm h-1/6 w-full">
       <input
         type="text"
-        name="UserID"
-        id="UserID"
+        name="userNickName"
         className="focus:ring-gray-500 focus:border-gray-500 block w-full h-1/6 pl-7 pr-12 sm:text-sm border-gray-300 ring-gray-300 rounded-md"
-        placeholder="Search user ID"
+        placeholder="Search user Nickname"
         onChange={onChange}
+        value={user}
       />
       <button type="button" className="absolute ml-52 h-full" onClick={onClickHandler}>
         <SearchIcon className="hover:text-black w-full h-full"/>
