d11d05aedd9b3c76dad8ee2c730c21a92ad2c448
강명구 <mg5566@hanmail.net>
2021-10-12 15:12:35 +0900

    refactor: SearchUserNicknameInputBox 로 변경

--- components/SearchUserNicknameInputBox.tsx
+++ components/SearchUserNicknameInputBox.tsx
@@ -1,15 +1,15 @@
 import { SearchIcon } from "@heroicons/react/solid";
 import React, { useState } from "react";
 
-const SearchUserInputBox = () => {
-  const [user, setUser] = useState("");
+const SearchUserNicknameInputBox = () => {
+  const [userNickname, setUserNickname] = useState("");
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-    setUser(event.target.value);
+    setUserNickname(event.target.value);
   }
 
   const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("User", user);
+    console.log("User Nickname:", userNickname);
     // 향후 State User 를 사용하여 <Link href={`/profile/${User}`} /> 이렇게 하면 될듯?!
   };
 
@@ -21,7 +21,7 @@ const SearchUserInputBox = () => {
         className="focus:ring-gray-500 focus:border-gray-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 ring-gray-300 rounded-md"
         placeholder="Search user Nickname"
         onChange={onChange}
-        value={user}
+        value={userNickname}
       />
       <button type="button" className="absolute ml-52 h-full" onClick={onClickHandler}>
         <SearchIcon className="hover:text-black w-full h-full"/>
@@ -30,4 +30,4 @@ const SearchUserInputBox = () => {
   );
 };
 
-export default SearchUserInputBox;
+export default SearchUserNicknameInputBox;
