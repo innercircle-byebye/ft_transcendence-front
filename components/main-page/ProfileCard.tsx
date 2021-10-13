89eda7e1bd9f927707f08c1c4bca60d2fc0d81ac
강명구 <mg5566@hanmail.net>
2021-10-13 23:06:09 +0900

    refactor: design 수정 과 grey color 추가

--- components/main-page/ProfileCard.tsx
+++ components/main-page/ProfileCard.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const ProfileCard = () => {
   return (
-    <div className="bg-gray-200 flex flex-col w-1/5 space-y-2 text-center">
+    <div className="bg-gray-200 flex flex-col w-1/5 space-y-2 text-center rounded-2xl">
       <div className="flex justify-center">
         {/* 상위 width 1/5 의 1/2 를 적용하고, 중앙 정렬을 위해서 상위 div 설정했습니다. */}
-        <div className="border-4 border-light-blue-500 border-opacity-100 rounded-full w-1/2">
+        <div className="border-4 border-opacity-100 rounded-full w-1/2">
           <Image src="/Logo.png" alt="Profile Image" width="100" height="100" />
         </div>
       </div>
