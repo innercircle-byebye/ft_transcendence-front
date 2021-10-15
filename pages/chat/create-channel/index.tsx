8258d816ad692676d28826a93373fe9e61c344eb
jiwlee <ddazi9576@google.com>
2021-10-15 21:59:14 +0900

    feat: create-channel에 필요한 요소들 넣고 디자인

--- pages/chat/create-channel/index.tsx
+++ pages/chat/create-channel/index.tsx
@@ -1,13 +1,13 @@
 import Navbar from "@/components/Navbar";
 import { useRouter } from "next/router";
-import React, { ReactElement, useCallback, useEffect, useState } from "react";
+import React, { ReactElement, useCallback, useEffect, useRef, useState } from "react";
 
 const CreateChannel = () => {
   const router = useRouter();
   const [channelName, setChannelName] = useState('');
   const [maxMemberNum, setMaxMemberNum] = useState(3);
-  const [isPrivate, setIsPrivate] = useState(false);
-  const [inviteMembers, setInviteMembers] = useState([]);
+  const [isPrivate, setIsPrivate] = useState(true);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const onChangeChannelName = useCallback((e) => {
     setChannelName(e.target.value);
@@ -15,6 +15,11 @@ const CreateChannel = () => {
 
   const onChangeMaxMemberNum = useCallback((e) => {
     setMaxMemberNum(e.target.value);
+  }, []);
+
+  const onClickSwitch = useCallback((e) => {
+    e.preventDefault();
+    setIsPrivate((prev) => !prev);
   }, []);
 
   const onClickCancel = useCallback(() => {
@@ -28,39 +33,45 @@ const CreateChannel = () => {
   }, [maxMemberNum]);
 
   return (
-    <div className="w-screen h-full flex flex-col items-center justify-center">
-      <div className="text-5xl">Create Chat Channel</div>
-      <input className="px-6 py-4 rounded-full bg-gray-100 text-xl" placeholder="채널명" type="text" value={channelName} onChange={onChangeChannelName} />
-      <div>
-        최대인원수
-        <input type="number" min={3} value={maxMemberNum} onChange={onChangeMaxMemberNum} />
-      </div>
-      <div>
-        <div>Private / Public</div>
-        <div className="flex items-center justify-center w-full mb-12">
-          <label className="flex items-center cursor-pointer">
-            <div className="relative">
-              <input type="checkbox" className="sr-only checked:translate-x-full" />
-              <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
-              <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
-            </div>
-          </label>
+    <div className="w-screen h-full flex flex-col items-center space-y-20">
+      <div className="mt-20 text-5xl">Create Chat Channel</div>
+      <div className="flex flex-col items-center justify-evenly space-y-10">
+        <input className="px-6 py-4 w-56 rounded-full bg-gray-100 text-xl" placeholder="채널명" type="text" value={channelName} onChange={onChangeChannelName} />
+        <div className="flex flex-row items-center space-x-6">
+          <div className="ml-3 text-gray-700 font-medium">최대인원수</div>
+          <input className="px-6 py-4 w-24 rounded-full bg-gray-100 text-xl" type="number" min={3} value={maxMemberNum} onChange={onChangeMaxMemberNum} />
         </div>
-      </div>
-      <div className="space-x-4">
-        <button
-          className="bg-gray-400 text-white py-3 px-8 rounded-full focus:outline-none focus:shadow-outline"
-          type="button"
-          onClick={onClickCancel}
-        >
-          CANCEL
+
+
+        <div className="flex flex-row items-center justify-center space-x-6">
+          <div className="ml-3 text-gray-700 font-medium">
+            Public / Private
+          </div>
+          <button className="relative" onClick={onClickSwitch}>
+            {/* <input type="checkbox" id="toggleB" className="sr-only" /> */}
+            <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
+            {isPrivate ? (<div className="absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>) :
+              (<div className="absolute right-1 top-1 bg-red-400 w-6 h-6 rounded-full transition"></div>)}
+          </button>
+        </div>
+
+
+        <div>invite member</div>
+        <div className="space-x-4">
+          <button
+            className="bg-gray-400 text-white py-3 px-8 rounded-full focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={onClickCancel}
+          >
+            CANCEL
+          </button>
+          <button
+            className="bg-amber-600 text-white py-3 px-10 rounded-full focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            SAVE
         </button>
-        <button
-          className="bg-amber-600 text-white py-3 px-10 rounded-full focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          SAVE
-        </button>
+        </div>
       </div>
     </div>
   );
@@ -72,7 +83,7 @@ CreateChannel.getLayout = function getLayout(page: ReactElement) {
       <div className="flex-initial">
         <Navbar />
       </div>
-      <div className="bg-gray-300 flex flex-row p-4 space-x-4 h-auto flex-1">
+      <div className="bg-gray-300 flex flex-row h-auto flex-1">
         <main>{page}</main>
       </div>
     </div>
