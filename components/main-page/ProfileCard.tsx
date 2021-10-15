b9ec7ce7e8f92d6b5e82fbe4999435f3a140eac8
강명구 <mg5566@hanmail.net>
2021-10-15 13:33:14 +0900

    feat: ProfileCard Component props 적용 및 test

--- components/main-page/ProfileCard.tsx
+++ components/main-page/ProfileCard.tsx
@@ -1,15 +1,21 @@
+import { IUser } from "@/typings/db";
 import Image from "next/image";
+import React from "react";
 
-const ProfileCard = () => {
+const ProfileCard: React.FC<IUser> = (userData: IUser) => {
+  console.log("userData:", userData);
+  console.log(userData.imagePath)
   return (
     <div className="bg-gray-200 flex flex-col w-1/5 space-y-2 text-center rounded-2xl">
       <div className="flex justify-center">
         {/* 상위 width 1/5 의 1/2 를 적용하고, 중앙 정렬을 위해서 상위 div 설정했습니다. */}
         <div className="border-4 border-opacity-100 rounded-full w-1/2">
-          <Image src="/Logo.png" alt="Profile Image" width="100" height="100" />
+          {/* <Image src="/Logo.png" alt="Profile Image" width="100" height="100" /> */}
+          <Image src={`${userData.imagePath}`} alt="Profile Image" width="100" height="100"/>
         </div>
       </div>
-      <div className="p-1">name</div>
+      {/* <div className="p-1">name</div> */}
+      <div className="p-1">{userData.nickname}</div>
       <div className="p-1">History</div>
       <div className="p-1">rate</div>
     </div>
