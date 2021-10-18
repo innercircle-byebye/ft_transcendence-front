ad3e483738ac9f38686b17962c7aa3631afef703
강명구 <mg5566@hanmail.net>
2021-10-18 22:35:49 +0900

    chore: modal 적용

--- components/main-page/AnnouncementList.tsx
+++ components/main-page/AnnouncementList.tsx
@@ -1,15 +1,27 @@
 import { IAnnouncement } from "@/typings/db";
 import fetcher from "@/utils/fetcher";
 import { SpeakerphoneIcon } from "@heroicons/react/outline";
+import { useState } from "react";
 import useSWR from "swr";
+import AnnouncementModal from "./AnnouncementModal";
 
 const AnnouncementList = () => {
   // api 호출
   // GET http://localhost:3005/api/admin/announcement
-  const { data: announcementData } = useSWR<IAnnouncement[]>(  // api 의 결과값이 array
+  const { data: announcementData } = useSWR<IAnnouncement[]>( // api 의 결과값이 array
     "/api/admin/announcement",
     fetcher
   );
+
+  const [clickedIndex, setClickedIndex] = useState<number | null>(null);
+
+  const onClickMethod = (id: number) => {
+    setClickedIndex(id);
+  };
+
+  const onCloseModal = () => {
+    setClickedIndex(null);
+  };
 
   return (
     <div className="bg-gray-700 flex flex-col w-1/5 text-center py-6 space-y-6 rounded-2xl">
@@ -23,18 +35,22 @@ const AnnouncementList = () => {
       <div className="flex flex-col space-y-4 mx-4 rounded-lg">
         {/* 향후 list 로 관리 scroll 기능으로 확장 예정 */}
         <div className="h-8 overflow-y-auto space-y-3">
-        {announcementData?.map((item: IAnnouncement) => (
-          <div key={item.announcementId} className="bg-sky-200 rounded-md py-2">
-              <a data-toggle="modal" href="#AnnouncementModal">{item.title}</a>
-          </div>
-        ))}
+          {announcementData?.map((item: IAnnouncement) => (
+            <div
+              className="bg-sky-200 rounded-md py-2"
+              key={item.announcementId}
+            >
+              <button onClick={() => onClickMethod(item.announcementId)}>
+                <a>{item.title}</a>
+              </button>
+              <AnnouncementModal
+                item={item}
+                isShow={clickedIndex === item.announcementId}
+                onCloseModal={onCloseModal}
+              />
+            </div>
+          ))}
         </div>
-        {/* align-middle 이 적용되지 않아서 y축 padding 을 적용하여 text 를 중앙정렬시켰습니다. */}
-        {/*
-        <div className="bg-sky-200 rounded-md py-2">list1</div>
-        <div className="bg-sky-200 rounded-md py-2">list2</div>
-        <div className="bg-sky-200 rounded-md py-2">list3</div>
-        */}
       </div>
     </div>
   );
