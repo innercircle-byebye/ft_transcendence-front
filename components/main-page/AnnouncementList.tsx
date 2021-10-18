285af66a23d0b43dfd0b788af3c9e4e3b3a9181b
강명구 <mg5566@hanmail.net>
2021-10-18 13:43:19 +0900

    fix: type 을 변경하여 map 사용가능하도록 수정

--- components/main-page/AnnouncementList.tsx
+++ components/main-page/AnnouncementList.tsx
@@ -7,20 +7,10 @@ import useSWR from "swr";
 const AnnouncementList = () => {
   // api 호출
   // GET http://localhost:3005/api/admin/announcement
-  /* 안되니까 SWR 로 변경해보자
-  const announcementData: IAnnouncement = axios
-    .get(`${process.env.BACK_API_PATH}/api/admin/announcement`, {
-      withCredentials: true,
-    })
-    .then((res) => res.data);
-  */
-
-  const { data: announcementData } = useSWR<IAnnouncement>(
+  const { data: announcementData } = useSWR<IAnnouncement[]>(  // api 의 결과값이 array
     "/api/admin/announcement",
     fetcher
   );
-  console.log("Announcement:", announcementData);
-  // console.log(Object.entries(announcementData));  이 코드는 error 가 발생합니다.
 
   return (
     <div className="bg-gray-700 flex flex-col w-1/5 text-center py-6 space-y-6 rounded-2xl">
@@ -33,7 +23,6 @@ const AnnouncementList = () => {
       </div>
       <div className="flex flex-col space-y-4 mx-4 rounded-lg">
         {/* 향후 list 로 관리 scroll 기능으로 확장 예정 */}
-        {/* 객체를 array 로 바꿔야하는 군요... 할 수 있을 것 같은데... 일단 error 없이 되긴함 */}
         {announcementData?.map((item: IAnnouncement) => (
           <div key={item.announcementId} className="bg-sky-200 rounded-md py-2">
               <a>{item.title}</a>
