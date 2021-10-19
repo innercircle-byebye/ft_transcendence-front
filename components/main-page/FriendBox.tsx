6e3977b06e76ce4ab762615551cda35b084c5041
강명구 <mg5566@hanmail.net>
2021-10-19 17:18:39 +0900

    style: 친구목록 component design

---
+++ components/main-page/FriendBox.tsx
@@ -0,0 +1,26 @@
+import { VFC } from 'react';
+import { IUser } from '@/typings/db';
+
+interface IProps {
+  item: IUser;
+}
+
+const FriendBox: VFC<IProps> = ({ item }) => (
+  // justify-between 을 적용하여 양쪽으로 퍼지게
+  <div className="py-1 px-4 bg-amber-200 rounded-md flex w-full justify-between">
+    <div className="flex space-x-1">
+      <div>
+        {item.nickname}
+      </div>
+      <div>
+        ICON
+      </div>
+    </div>
+    <div className="space-x-2 text-white">
+      <button className="bg-sky-700 py-1 px-1 text-sm">게임신청</button>
+      <button className="bg-sky-700 py-1 px-1 text-sm">DM보내기</button>
+    </div>
+  </div>
+);
+
+export default FriendBox;
---
+++ components/main-page/MyFriendsList.tsx
@@ -0,0 +1,34 @@
+import useSWR from 'swr';
+import FriendBox from '@/components/main-page/FriendBox';
+import { IUser } from '@/typings/db';
+import fetcher from '@/utils/fetcher';
+
+const MyFriendsList = () => {
+  // 이건 어떻게 해결해야합니까?
+  /*
+  const userData: IUser[] = axios
+    .get('http://localhost:3000/api/members')
+    .then((response) => response.data);
+  */
+  const { data: userData } = useSWR<IUser[]>(
+    'http://localhost:3000/api/members',
+    fetcher,
+  );
+
+  return (
+    <div className="rounded-xl bg-sky-700 text-center flex flex-col space-y-2 w-1/4">
+      {/* title */}
+      <div className="text-white py-2">친구 목록</div>
+      {/* content list */}
+      <div className="flex flex-col space-y-4 mx-4 py-2">
+        <div className="space-y-2">
+          {userData?.map((item: IUser) => (
+            <FriendBox key={item.userId} item={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyFriendsList;
