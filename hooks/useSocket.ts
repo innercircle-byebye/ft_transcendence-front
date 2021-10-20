074558fba4a051b64a9a13a2354853a111378ea6
jiwlee <ddazi9576@google.com>
2021-10-20 15:25:34 +0900

    feat:useSocket 생성

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,22 +1,24 @@
-import io from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 import { useCallback } from 'react';
 
-const backUrl = 'http://localhost:3095';
+const backUrl = 'http://back-nestjs:3095';
 
-const sockets: { [key: string]: SocketIOClient.Socket } = {};
-const useSocket = (workspace?: string): [SocketIOClient.Socket | undefined, () => void] => {
-  console.log('rerender', workspace);
+const sockets: { [key: string]: Socket } = {};
+
+const useSocket = (workspace?: string): [Socket | undefined, () => void] => {
   const disconnect = useCallback(() => {
     if (workspace) {
       sockets[workspace].disconnect();
       delete sockets[workspace];
     }
   }, [workspace]);
+
   if (!workspace) {
     return [undefined, disconnect];
   }
+
   if (!sockets[workspace]) {
-    sockets[workspace] = io.connect(`${backUrl}/ws-${workspace}`, {
+    sockets[workspace] = io(`${backUrl}/ws-${workspace}`, {
       transports: ['websocket'],
     });
   }
