9a3c6837597b1d85225ece11e5ff7317b8a6f2cb
jiwlee <ddazi9576@google.com>
2021-10-21 23:21:22 +0900

    feat: chat page에 들어가면 소켓 연결하고 나오면 연결 끊는 기능 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,12 @@ import '@/styles/emoji.css';
 import '@/styles/mention.css';
 import type { AppProps } from 'next/app';
 import { NextPage } from 'next';
-import { ReactElement, ReactNode } from 'react';
+import {
+  ReactElement, ReactNode, useEffect, useState,
+} from 'react';
 import 'tailwindcss/tailwind.css';
+import { useRouter } from 'next/router';
+import useSocket from '@/hooks/useSocket';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,6 +20,24 @@ type AppPropsWithLayout = AppProps & {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
+  const { pathname } = router;
+  const [namespace, setNamespace] = useState<string | undefined>(undefined);
+  const [socket, disconnect] = useSocket(namespace);
+
+  useEffect(() => {
+    if (pathname === '/chat') {
+      setNamespace('chat');
+    } else {
+      setNamespace(undefined);
+    }
+  }, [namespace, pathname, socket]);
+
+  useEffect(() => () => {
+    disconnect();
+    console.log(`disconnect ${namespace}`);
+  }, [disconnect, namespace]);
+
   return getLayout(
     <Component {...pageProps} />,
   );
