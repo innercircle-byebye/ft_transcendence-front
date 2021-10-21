314fcd0494d1f6549f5371eed9691865f15e7727
jiwlee <ddazi9576@google.com>
2021-10-21 14:10:02 +0900

    chore: prettier 적용

--- layouts/ChatLayout.tsx
+++ layouts/ChatLayout.tsx
@@ -1,23 +1,23 @@
+import React, { FC } from 'react';
 import ChannelList from '@/components/chat-page/ChannelList';
 import DMList from '@/components/chat-page/DMList';
 import Navbar from '@/components/Navbar';
-import React, { FC } from 'react';
 
 const ChatLayout: FC = ({ children }) => (
-    <div className="h-screen flex flex-col">
-      <div className="flex-initial">
-        <Navbar />
-      </div>
-      <div className="bg-sky-100 flex flex-row p-4 space-x-4 h-auto flex-1">
-        <div className="flex flex-col space-y-4 w-60">
-          <ChannelList />
-          <div className="flex-1">
-            <DMList />
-          </div>
-        </div>
-        <main className="flex-1">{children}</main>
-      </div>
+  <div className="h-screen flex flex-col">
+    <div className="flex-initial">
+      <Navbar />
     </div>
+    <div className="bg-sky-100 flex flex-row p-4 space-x-4 h-auto flex-1">
+      <div className="flex flex-col space-y-4 w-60">
+        <ChannelList />
+        <div className="flex-1">
+          <DMList />
+        </div>
+      </div>
+      <main className="flex-1">{children}</main>
+    </div>
+  </div>
 );
 
 export default ChatLayout;
