8e6750cf87edfd367a129529b136d97dc4e29a04
강명구 <mg5566@hanmail.net>
2021-10-24 17:35:02 +0900

    feat: Enter 기능 추가

--- components/navigation-bar/SearchUserNicknameInputBox.tsx
+++ components/navigation-bar/SearchUserNicknameInputBox.tsx
@@ -1,5 +1,5 @@
+import React, { useCallback, useState } from 'react';
 import { SearchIcon } from '@heroicons/react/solid';
-import React, { useState } from 'react';
 
 const SearchUserNicknameInputBox = () => {
   const [userNickname, setUserNickname] = useState('');
@@ -8,11 +8,22 @@ const SearchUserNicknameInputBox = () => {
     setUserNickname(event.target.value);
   };
 
-  // const onClickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
   const onClickHandler = () => {
     console.log('User Nickname:', userNickname);
     // 향후 State User 를 사용하여 <Link href={`/profile/${User}`} /> 이렇게 하면 될듯?!
   };
+
+  const onKeyPressHandler = useCallback(
+    (e) => {
+      if (e.key === 'Enter') {
+        if (userNickname !== '') {
+          onClickHandler();
+        }
+      }
+    },
+    // 이 부분이 없으면 계속 빈칸만 나옵니다...
+    [userNickname],
+  );
 
   return (
     <div className="mt-1 relative flex item-center rounded-md shadow-sm w-full text-gray-900">
@@ -22,9 +33,14 @@ const SearchUserNicknameInputBox = () => {
         className="focus:ring-gray-500 focus:border-gray-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 ring-gray-300 rounded-md"
         placeholder="Search user Nickname"
         onChange={onChange}
+        onKeyPress={onKeyPressHandler}
         value={userNickname}
       />
-      <button type="button" className="absolute ml-52 h-full" onClick={onClickHandler}>
+      <button
+        type="button"
+        className="absolute ml-52 h-full"
+        onClick={onClickHandler}
+      >
         <SearchIcon className="hover:text-black w-full h-full text-gray-900" />
       </button>
     </div>
