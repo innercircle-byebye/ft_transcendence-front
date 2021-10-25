e3c88fa64f8acd9322b6409e1827b287c77306ba
jiwlee <ddazi9576@google.com>
2021-10-25 15:01:51 +0900

    refactor: trigger @로 변경

--- pages/chat/create-channel/index.tsx
+++ pages/chat/create-channel/index.tsx
@@ -223,14 +223,14 @@ const CreateChannel = () => {
         )}
         <div className="col-span-2 flex flex-col space-y-5 items-center">
           <form className="w-80 flex items-center justify-evenly flex-row">
-            <div className="w-52 bg-gray-100 rounded-full pl-3 pt-1">
+            <div className="w-56 bg-gray-100 rounded-full pl-3 pt-1">
               <div className="flex flex-row justify-evenly">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mt-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
                 <MentionMember
-                  trigger=""
-                  placeholder="초대할 멤버 닉네임"
+                  trigger="@"
+                  placeholder="@초대할 멤버 닉네임"
                   value={inviteMember}
                   onChangeValue={onChangeInviteMember}
                   inputRef={textareaRef}
