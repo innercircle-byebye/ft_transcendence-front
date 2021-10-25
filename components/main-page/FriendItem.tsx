ac2f074518ce5ed36a207c43a47fe5ffe495477c
강명구 <mg5566@hanmail.net>
2021-10-25 17:06:30 +0900

    refactor: 온라인 상태를 확인하는 조건문 수정

--- components/main-page/FriendItem.tsx
+++ components/main-page/FriendItem.tsx
@@ -1,33 +1,25 @@
 import { VFC } from 'react';
-// import { IUser } from '@/typings/db';
 
 interface IProps {
-  // item: IUser;
   nickname: string;
-  status: string;
 }
 
-const FriendItem: VFC<IProps> = ({ nickname, status }) => {
-  if (status === 'offline') return null;
-  if (status === 'not_registed') return null;
-
-  return (
-    // justify-between 을 적용하여 양쪽으로 퍼지게
-    <div className="py-1 px-4 bg-amber-200 rounded-md flex w-full justify-between">
-      <div className="flex space-x-1">
-        <div>{nickname}</div>
-        <div>ICON</div>
-      </div>
-      <div className="space-x-2 text-white">
-        <button type="button" className="bg-sky-700 py-1 px-1 text-sm">
-          게임신청
-        </button>
-        <button type="button" className="bg-sky-700 py-1 px-1 text-sm">
-          DM보내기
-        </button>
-      </div>
+const FriendItem: VFC<IProps> = ({ nickname }) => (
+  // justify-between 을 적용하여 양쪽으로 퍼지게
+  <div className="py-1 px-4 bg-amber-200 rounded-md flex w-full justify-between">
+    <div className="flex space-x-1">
+      <div>{nickname}</div>
+      <div>ICON</div>
     </div>
-  );
-};
+    <div className="space-x-2 text-white">
+      <button type="button" className="bg-sky-700 py-1 px-1 text-sm">
+        게임신청
+      </button>
+      <button type="button" className="bg-sky-700 py-1 px-1 text-sm">
+        DM보내기
+      </button>
+    </div>
+  </div>
+);
 
 export default FriendItem;
--- components/main-page/OnlineFriendList.tsx
+++ components/main-page/OnlineFriendList.tsx
@@ -4,12 +4,7 @@ import { IUser } from '@/typings/db';
 import fetcher from '@/utils/fetcher';
 
 const OnlineFriendList = () => {
-  const { data: userData } = useSWR<IUser[]>(
-    '/api/friend/list',
-    fetcher,
-  );
-
-  console.log('userData:', userData);
+  const { data: userData } = useSWR<IUser[]>('/api/friend/list', fetcher);
 
   return (
     <div className="rounded-xl bg-sky-700 text-center flex flex-col space-y-2 w-1/4">
@@ -18,13 +13,12 @@ const OnlineFriendList = () => {
       {/* content list */}
       <div className="flex flex-col space-y-4 mx-4 py-2">
         <div className="space-y-2">
-          {userData?.map((item: IUser) => (
-            <FriendItem
-              key={item.userId}
-              nickname={item.nickname}
-              status={item.status}
-            />
-          ))}
+          {userData?.map((item: IUser) => {
+            if (item.status === 'offline' || item.status === 'not_registed') {
+              return null;
+            }
+            return <FriendItem key={item.userId} nickname={item.nickname} />;
+          })}
         </div>
       </div>
     </div>
