282822530b61a74f30fdd6fd78f26d4ebe3eed53
강명구 <mg5566@hanmail.net>
2021-10-25 17:20:02 +0900

    refactor: userData->friendData 로 수정

--- components/main-page/OnlineFriendList.tsx
+++ components/main-page/OnlineFriendList.tsx
@@ -4,7 +4,7 @@ import { IUser } from '@/typings/db';
 import fetcher from '@/utils/fetcher';
 
 const OnlineFriendList = () => {
-  const { data: userData } = useSWR<IUser[]>('/api/friend/list', fetcher);
+  const { data: friendData } = useSWR<IUser[]>('/api/friend/list', fetcher);
 
   return (
     <div className="rounded-xl bg-sky-700 text-center flex flex-col space-y-2 w-1/4">
@@ -13,7 +13,7 @@ const OnlineFriendList = () => {
       {/* content list */}
       <div className="flex flex-col space-y-4 mx-4 py-2">
         <div className="space-y-2">
-          {userData?.map((item: IUser) => {
+          {friendData?.map((item: IUser) => {
             if (item.status === 'offline' || item.status === 'not_registed') {
               return null;
             }
