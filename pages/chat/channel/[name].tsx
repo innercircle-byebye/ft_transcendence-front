e518bcf9830c61fd0546acdef0f6d9ac48a04e93
jiwlee <ddazi9576@google.com>
2021-10-27 21:53:49 +0900

    refactor: channel page pre-render 적용

--- pages/chat/channel/[name].tsx
+++ pages/chat/channel/[name].tsx
@@ -4,6 +4,7 @@ import React, {
 } from 'react';
 import useSWR from 'swr';
 import axios from 'axios';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import ChatBox from '@/components/chat-page/ChatBox';
 import ChatLayout from '@/layouts/ChatLayout';
 import useInput from '@/hooks/useInput';
@@ -14,22 +15,36 @@ import {
 import ChatItem from '@/components/chat-page/ChatItem';
 import useSocket from '@/hooks/useSocket';
 import ChannelButtons from '@/components/chat-page/ChannelButtons';
+import reissueToken from '@/utils/reissueTokens';
 
-const Channel = () => {
+const Channel = ({
+  userInitialData,
+  channelInitialData,
+  channelChatInitialData,
+  channelMemberInitialData,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const channelName = router.query.name;
   const [chat, onChangeChat, setChat] = useInput('');
   const [showEmoji, setShowEmoji] = useState(false);
   const { socket } = useSocket('chat');
-  const { data: userData } = useSWR<IUser>('/api/user/me', fetcher);
+  const { data: userData } = useSWR<IUser>('/api/user/me', fetcher, {
+    initialData: userInitialData,
+  });
   const { data: channelData } = useSWR<IChannel>(
-    `/api/channel/${channelName}`, fetcher,
+    `/api/channel/${channelName}`, fetcher, {
+      initialData: channelInitialData,
+    },
   );
   const { data: channelChatData, mutate: mutateChat } = useSWR<IChannelChat[]>(
-    `/api/channel/${channelName}/chat`, fetcher,
+    `/api/channel/${channelName}/chat`, fetcher, {
+      initialData: channelChatInitialData,
+    },
   );
   const { data: channelMemberData } = useSWR<IChannelMember[]>(
-    `/api/channel/${channelName}/member`, fetcher,
+    `/api/channel/${channelName}/member`, fetcher, {
+      initialData: channelMemberInitialData,
+    },
   );
 
   const onCloseEmoji = useCallback(() => {
@@ -126,6 +141,69 @@ const Channel = () => {
 
 Channel.getLayout = function getLayout(page: ReactElement) {
   return <ChatLayout>{page}</ChatLayout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const access_token = process.env.ACCESS_TOKEN || '';
+  const refresh_token = process.env.REFRESH_TOKEN || '';
+  const channelName = context.query.name;
+
+  if (
+    !context.req.cookies[refresh_token]
+    || !context.req.cookies[access_token]
+  ) {
+    return reissueToken(
+      context,
+      access_token,
+      refresh_token,
+      '/chat',
+    );
+  }
+
+  const userInitialData: IUser = await axios
+    .get(`http://back-nestjs:${process.env.BACK_PORT}/api/user/me`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `Authentication=${context.req.cookies[access_token]}`,
+      },
+    })
+    .then((response) => response.data);
+
+  const channelInitialData: IChannel = await axios
+    .get(`http://back-nestjs:${process.env.BACK_PORT}/api/channel/${channelName}`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `Authentication=${context.req.cookies[access_token]}`,
+      },
+    })
+    .then((response) => response.data);
+
+  const channelChatInitialData: IChannelChat[] = await axios
+    .get(`http://back-nestjs:${process.env.BACK_PORT}/api/channel/${channelName}/chat`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `Authentication=${context.req.cookies[access_token]}`,
+      },
+    })
+    .then((response) => response.data);
+
+  const channelMemberInitialData: IChannelMember[] = await axios
+    .get(`http://back-nestjs:${process.env.BACK_PORT}/api/channel/${channelName}/member`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `Authentication=${context.req.cookies[access_token]}`,
+      },
+    })
+    .then((response) => response.data);
+
+  return {
+    props: {
+      userInitialData,
+      channelInitialData,
+      channelChatInitialData,
+      channelMemberInitialData,
+    },
+  };
 };
 
 export default Channel;
