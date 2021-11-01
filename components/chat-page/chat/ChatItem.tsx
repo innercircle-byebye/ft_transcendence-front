9403293c70865bbb5181f1b2c4162514fc526755
jiwlee <ddazi9576@google.com>
2021-11-01 18:01:42 +0900

    style: ChatItem 꾸미기

--- components/chat-page/chat/ChatItem.tsx
+++ components/chat-page/chat/ChatItem.tsx
@@ -1,24 +1,58 @@
-import { FC } from 'react';
-// import Image from 'next/image';
+import React, { FC, useMemo } from 'react';
+import Image from 'next/image';
+import regexifyString from 'regexify-string';
+import Link from 'next/link';
+import dayjs from 'dayjs';
 import { IChannelChat } from '@/typings/db';
 
 interface Props {
   chatData: IChannelChat;
 }
 
-const ChatItem: FC<Props> = ({ chatData }) => (
-  <div className="flex flex-row">
-    {/* <div className="relative bg-blue-300 w-5 h-5 rounded-full shadow-lg mr-2">
-      <Image
-        src={user.imagePath}
-        alt="previewImage"
-        objectFit="cover"
-        layout="fill"
-        className="rounded-full"
-      />
-    </div> */}
-    {`${chatData.createdAt} ${chatData.userId} ${chatData.content}`}
-  </div>
-);
+const ChatItem: FC<Props> = ({ chatData }) => {
+  const result = useMemo(
+    () => (
+      regexifyString({
+        input: chatData.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/);
+          if (arr) {
+            return (
+              <Link key={match + index} href={`/chat/dm/${arr[1]}`}>
+                <a className="bg-amber-400">
+                  @
+                  {arr[1]}
+                </a>
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      })
+    ),
+    [chatData.content],
+  );
 
+  return (
+    <div className="flex flex-row">
+      <div className="relative bg-blue-300 w-10 h-10 mr-2">
+        <Image
+          src={chatData.user.imagePath}
+          alt="previewImage"
+          objectFit="cover"
+          layout="fill"
+          className="rounded-md"
+        />
+      </div>
+      <div>
+        <div className="space-x-2">
+          <b>{chatData.user.nickname}</b>
+          <span className="text-sm text-gray-700">{dayjs(chatData.createdAt).format('h:mm A')}</span>
+        </div>
+        <p>{result}</p>
+      </div>
+    </div>
+  );
+};
 export default ChatItem;
