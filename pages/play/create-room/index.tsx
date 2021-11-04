921e40768a4e1a646d3b0f113667e0d46f77e861
jiwlee <ddazi9576@google.com>
2021-11-04 17:57:41 +0900

    feat: 방만들기 api 적용

--- pages/play/create-room/index.tsx
+++ pages/play/create-room/index.tsx
@@ -1,13 +1,21 @@
 import { useRouter } from 'next/dist/client/router';
-import React, { ReactElement, useCallback, useState } from 'react';
+import React, {
+  ReactElement, useCallback, useEffect, useState,
+} from 'react';
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import useInput from '@/hooks/useInput';
 import SwitchPublicPrivate from '@/components/chat-page/common/SwitchPublicPrivate';
 import Navbar from '@/components/navigation-bar/Navbar';
+import { IGameRoom } from '@/typings/db';
 
-const CreateRoom = () => {
+const CreateRoom = ({ allRoomList }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const [roomName, onChangeRoomName] = useInput('');
+  const [roomNameError, setRoomNameError] = useState(false);
   const [difficulty, onChangeDifficulty] = useInput(0);
+  const [ballSpeed, setBallSpeed] = useState('slow');
   const [winScore, onChangeWinScore] = useInput(5);
   const [numOfSpectator, onChangeNumOfSpectator] = useInput(5);
   const [isPrivate, setIsPrivate] = useState(false);
@@ -19,14 +27,54 @@ const CreateRoom = () => {
   }, [router]);
 
   const onClickMake = useCallback(() => {
-    router.push('/play');
-  }, [router]);
+    axios.post('/api/game/room', {
+      title: roomName,
+      password: isPrivate ? password : null,
+      maxParticipantNum: numOfSpectator,
+      winPoint: winScore,
+      ballSpeed,
+    }, {
+      headers: {
+        withCredentials: 'true',
+      },
+    }).then(() => {
+      router.push('/play');
+    }).catch(() => {
+      toast.error('방만들기에 실패했습니다.', { position: 'bottom-right', theme: 'colored' });
+    });
+  }, [ballSpeed, isPrivate, numOfSpectator, password, roomName, router, winScore]);
+
+  useEffect(() => {
+    if (difficulty === 0) { setBallSpeed('slow'); }
+    if (difficulty === 1) { setBallSpeed('medium'); }
+    if (difficulty === 2) { setBallSpeed('fast'); }
+  }, [difficulty]);
+
+  useEffect(() => {
+    if (allRoomList.map((v: IGameRoom) => v.title).includes(roomName)) {
+      setRoomNameError(true);
+    } else {
+      setRoomNameError(false);
+    }
+  }, [allRoomList, roomName]);
 
   return (
     <div className="flex-1 bg-gray-200">
       <div className="flex flex-col items-center gap-10 pt-28">
         <p className="text-5xl">Create Game Room</p>
-        <input type="text" placeholder="방이름" value={roomName} onChange={onChangeRoomName} className="col-span-2 w-80 px-6 py-4 rounded-full bg-gray-100 text-xl outline-none" />
+        <div className="relative">
+          <input type="text" placeholder="방이름" value={roomName} onChange={onChangeRoomName} className="col-span-2 w-80 px-6 py-4 rounded-full bg-gray-100 text-xl outline-none" />
+          {!roomName.trim().length && (
+          <div className="absolute left-5 text-red-500 text-xs italic">
+            채널명을 입력해주세요
+          </div>
+          )}
+          {roomNameError && (
+          <div className="absolute left-5 text-red-500 text-xs italic">
+            이미 존재하는 채널명입니다.
+          </div>
+          )}
+        </div>
         <form className="grid grid-cols-2 gap-8 items-center">
           <span>
             난이도(하 / 중 / 상)
@@ -67,12 +115,13 @@ const CreateRoom = () => {
             className="bg-amber-600 text-white py-3 px-10 rounded-full focus:outline-none focus:shadow-outline"
             type="button"
             onClick={onClickMake}
-            disabled={isPrivate && passwordError}
+            disabled={!roomName.trim().length || roomNameError || (isPrivate && passwordError)}
           >
             SAVE
           </button>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
@@ -86,6 +135,29 @@ CreateRoom.getLayout = function getLayout(page: ReactElement) {
       {page}
     </div>
   );
+};
+
+interface IProps {
+  allRoomList: IGameRoom[];
+}
+
+export const getServerSideProps: GetServerSideProps<IProps> = async (context) => {
+  const access_token = process.env.ACCESS_TOKEN || '';
+
+  const allRoomList: IGameRoom[] = await axios
+    .get(`http://back-nestjs:${process.env.BACK_PORT}/api/game/room/list`, {
+      withCredentials: true,
+      headers: {
+        Cookie: `Authentication=${context.req.cookies[access_token]}`,
+      },
+    })
+    .then((response) => response.data);
+
+  return {
+    props: {
+      allRoomList,
+    },
+  };
 };
 
 export default CreateRoom;
