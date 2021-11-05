d04c9eb9100fb6f60d04247aee7444009897af36
jiwlee <ddazi9576@google.com>
2021-11-05 17:55:13 +0900

    feat: player1, player2 적용

--- components/play-page/EntranceModal.tsx
+++ components/play-page/EntranceModal.tsx
@@ -1,5 +1,5 @@
 import {
-  Dispatch, SetStateAction, useCallback, VFC,
+  Dispatch, SetStateAction, useCallback, useState, VFC,
 } from 'react';
 import { IGameRoom } from '@/typings/db';
 
@@ -9,6 +9,9 @@ interface IProps {
 }
 
 const EntranceModal: VFC<IProps> = ({ roomInfo, setRoomToEntrance }) => {
+  const [player1] = useState(roomInfo.gameMembers.find((v) => v.status === 'player1'));
+  const [player2] = useState(roomInfo.gameMembers.find((v) => v.status === 'player2'));
+
   const onClickCloseModal = useCallback(() => {
     setRoomToEntrance(null);
   }, [setRoomToEntrance]);
@@ -23,17 +26,15 @@ const EntranceModal: VFC<IProps> = ({ roomInfo, setRoomToEntrance }) => {
 
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <div className="relative bg-amber-50 p-10 rounded-xl space-y-2">
+      <div className="relative bg-amber-50 p-8 rounded-xl space-y-2">
         <button type="button" onClick={onClickCloseModal}>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 absolute top-3 right-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
         <p className={`${roomInfo.title.length > 10 ? 'text-3xl' : 'text-4xl'}`}>{`#${roomInfo.gameRoomId}  ${roomInfo.title}`}</p>
-        <p className="text-center text-xl">
-          {roomInfo.gameMembers.at(0)?.nickname}
-          {' vs '}
-          {roomInfo.gameMembers.at(1)?.nickname}
+        <p className="text-center text-2xl">
+          {`${player1 ? player1.nickname : ''} vs ${player2 ? player2.nickname : ''}`}
         </p>
         <div className="flex justify-evenly">
           <button type="button" onClick={onClickWatch} className={`${roomInfo.currentMemberCount === roomInfo.maxParticipantNum ? 'bg-gray-400' : 'bg-green-500'} p-2 rounded-xl`}>
