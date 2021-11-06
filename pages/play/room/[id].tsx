3110a162111050ec4ca5ccce9ad0e8417350b2e4
강명구 <mg5566@hanmail.net>
2021-11-06 17:44:13 +0900

    style: Room 3개 버튼 component design 구현 및 적용

--- pages/play/room/[id].tsx
+++ pages/play/room/[id].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import GameScreen from '@/components/play-room-page/GameScreen';
+import RoomButtonList from '@/components/play-room-page/RoomButtonList';
 
 const Room = () => {
   const router = useRouter();
@@ -15,8 +16,11 @@ const Room = () => {
       </div>
       {/* info screen */}
       <div className="w-1/4 bg-amber-100">
+        {/* 제목이 수평 기준으로 center 정렬이 되는데, 수직기준으로 center 정렬이 안됩니다... 어찌하는 거지?! */}
         <div className="bg-gray-400 h-1/12 text-center">
-          {`# ${roomNumber} 방제목 api 로 받아서 사용하시오`}
+          <div>
+            {`# ${roomNumber} 방제목 api 로 받아서 사용하시오`}
+          </div>
         </div>
         <div className="bg-red-300 h-1/4">
           {/* player Info */}
@@ -24,11 +28,12 @@ const Room = () => {
         </div>
         <div className="bg-green-300 h-1/12">
           {/* play room buttons */}
-          3button opt & replace & exit
+          {/* 3button opt & replace & exit */}
+          <RoomButtonList />
         </div>
         <div className="bg-sky-300 h-7/12">
           {/* participant chatting swap button */}
-          <div className="flex h-1/12">
+          <div className="flex h-1/12 text-center">
             <button
               type="button"
               onClick={() => setIsChatting(true)}
