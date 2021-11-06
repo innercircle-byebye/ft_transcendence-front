69d4a6cd007b53f182acfca7e06cc060c92287bd
강명구 <mg5566@hanmail.net>
2021-11-06 16:58:02 +0900

    style: participant chatting swap button 추가

--- pages/play/room/[id].tsx
+++ pages/play/room/[id].tsx
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 import GameScreen from '@/components/play-room-page/GameScreen';
 
 const Room = () => {
   const router = useRouter();
   const roomNumber = router.query.id;
+  const [isChatting, setIsChatting] = useState(true);
 
   return (
     <div className="flex justify-center">
@@ -17,13 +19,42 @@ const Room = () => {
           {`# ${roomNumber} 방제목 api 로 받아서 사용하시오`}
         </div>
         <div className="bg-red-300 h-1/4">
+          {/* player Info */}
           player 1 & player 2
         </div>
         <div className="bg-green-300 h-1/12">
+          {/* play room buttons */}
           3button opt & replace & exit
         </div>
         <div className="bg-sky-300 h-7/12">
-          observer | chatting
+          {/* participant chatting swap button */}
+          <div className="flex h-1/12">
+            <button
+              type="button"
+              onClick={() => setIsChatting(true)}
+              className={`w-1/2 ${isChatting && 'bg-sky-200'} ${!isChatting && 'bg-gray-400'}`}
+            >
+              chatting
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsChatting(false)}
+              className={`w-1/2 ${!isChatting && 'bg-sky-200'} ${isChatting && 'bg-gray-400'}`}
+            >
+              participant
+            </button>
+          </div>
+          <div>
+            {isChatting ? (
+              <div>
+                채팅입니다.
+              </div>
+            ) : (
+              <div>
+                관전자입니다.
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
