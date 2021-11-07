f459e804b5a6a89eea62081112f974b4d70a678e
강명구 <mg5566@hanmail.net>
2021-11-08 01:44:57 +0900

    style: GageScreen 에 ready button 추가

--- components/play-room-page/GameScreen.tsx
+++ components/play-room-page/GameScreen.tsx
@@ -1,4 +1,16 @@
-const GameScreen = () => {
+import { VFC } from 'react';
+
+interface IProps {
+  onClickReady1P: () => void;
+  onClickReady2P: () => void;
+  isReady1P: boolean;
+  isReady2P: boolean;
+  role: string;
+}
+
+const GameScreen: VFC<IProps> = ({
+  onClickReady1P, onClickReady2P, isReady1P, isReady2P, role,
+}) => {
   // 향후 props 를 통해서 이름을 받아온다.
   const player1 = 'mykang';
   const player2 = 'kycho';
@@ -20,8 +32,24 @@ const GameScreen = () => {
           {`${player2}`}
         </div>
       </div>
-      <div className="bg-gray-400 h-11/12">
-        game screen
+      <div className="bg-gray-400 h-11/12 justify-between items-center">
+        {/* game screen */}
+        <button
+          type="button"
+          onClick={onClickReady1P}
+          className={`w-1/5 h-1/5 rounded-full ${isReady1P && 'bg-amber-500'} ${!isReady1P && 'bg-gray-400'}`}
+          disabled={role !== '1P'}
+        >
+          ready!
+        </button>
+        <button
+          type="button"
+          onClick={onClickReady2P}
+          className={`w-1/5 h-1/5 rounded-full ${isReady2P && 'bg-amber-500'} ${!isReady2P && 'bg-gray-400'}`}
+          disabled={role !== '2P'}
+        >
+          ready!
+        </button>
       </div>
     </div>
   );
