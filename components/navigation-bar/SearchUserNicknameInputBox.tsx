8e7ceb6ddd547de15123a439b24a89c86b496174
jiwlee <ddazi9576@google.com>
2021-11-08 19:10:58 +0900

    feat: onKeyDown 함수 작성 & 적용

--- components/navigation-bar/SearchUserNicknameInputBox.tsx
+++ components/navigation-bar/SearchUserNicknameInputBox.tsx
@@ -1,5 +1,5 @@
 import React, {
-  useCallback, useEffect, useRef, useState, VFC,
+  useCallback, useEffect, useRef, VFC,
 } from 'react';
 import { SearchIcon } from '@heroicons/react/solid';
 import useSWR from 'swr';
@@ -8,58 +8,58 @@ import regexifyString from 'regexify-string';
 import MentionMember from '../chat-page/common/MentionMember';
 import fetcher from '@/utils/fetcher';
 import { ISimpleUser, IUser } from '@/typings/db';
-// import useInput from '@/hooks/useInput';
-// import router from 'next/router';
+import useInput from '@/hooks/useInput';
 
 const SearchUserNicknameInputBox: VFC = () => {
   const router = useRouter();
-  const [userNickname, setUserNickname] = useState('');
+  const [search, onChangeSearch] = useInput('');
   const { data: allUserData } = useSWR<IUser[]>('/api/user/all', fetcher);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const onKeyPress = useCallback((e) => {
-    if (e.key === 'Enter') {
-      router.push('/play');
-    }
-  }, [router]);
-
-  const onChangeUserNickname = useCallback((e) => {
-    setUserNickname(e.target.value);
-  }, []);
+  const onKeydown = useCallback(
+    (e) => {
+      if (e.key === 'Enter') {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          router.push(`/searches?search=${search}`);
+        }
+      }
+    },
+    [router, search],
+  );
 
   useEffect(() => {
     regexifyString({
-      input: userNickname,
+      input: search,
       pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
       decorator(match) {
         const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/);
         if (arr) {
-          router.push(`/search/${arr[1]}`);
-          setUserNickname('');
+          router.push(`/searches?search=${arr[1]}`);
           textareaRef.current?.blur();
           return (arr[1]);
         }
         return '';
       },
     });
-  }, [router, userNickname]);
+  }, [router, search]);
 
   return (
     <div className="flex flex-row items-center bg-white rounded-full px-3 py-1 gap-2">
       <SearchIcon className="hover:text-black w-6 h-6 text-gray-900" />
-      <div className="text-black w-60">
+      <div className="text-black w-32">
         <MentionMember
           trigger=""
-          value={userNickname}
-          onChangeValue={onChangeUserNickname}
-          placeholder="Search user Nickname"
+          value={search}
+          onChangeValue={onChangeSearch}
+          placeholder="Search ..."
           inputRef={textareaRef}
           data={
             allUserData?.map((user): ISimpleUser => ({
               userId: user.userId, nickname: user.nickname, imagePath: user.imagePath,
             }))
           }
-          onKeyPress={onKeyPress}
+          onKeyPress={onKeydown}
           className="navbar-mentions-input"
         />
       </div>
