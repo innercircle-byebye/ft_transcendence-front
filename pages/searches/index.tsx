1560fdc4b4290ba81db6ca37758db6bbd26b1c7d
jiwlee <ddazi9576@google.com>
2021-11-08 20:46:06 +0900

    feat: searches page 생성

--- pages/searches/index.tsx
+++ pages/searches/index.tsx
@@ -1,11 +1,44 @@
 import { useRouter } from 'next/router';
+import React, { ReactElement } from 'react';
+import useSWR from 'swr';
+import fetcher from '@/utils/fetcher';
+import { IUser } from '@/typings/db';
+import SearchProfileCard from '@/components/searches-page/SearchProfileCard';
+import PageContainer from '@/components/searches-page/PageContainer';
+import Navbar from '@/components/navigation-bar/Navbar';
+import ContentContainer from '@/components/searches-page/ContentContainer';
 
-const Search = () => {
+const Searches = () => {
   const router = useRouter();
+  const { search } = router.query;
+  const { data: allUserData } = useSWR<IUser[]>('/api/user/all', fetcher);
 
-  console.log(router);
-
-  return <div>search page</div>;
+  return (
+    <PageContainer>
+      <ContentContainer>
+        <p className="text-xl text-gray-700">{`Search '${search}'`}</p>
+        <div className="flex justify-center">
+          <div className="flex flex-wrap gap-7">
+            {
+              allUserData?.filter((data) => (data.nickname.includes(`${search}`)))
+                .map((user) => <SearchProfileCard key={user.userId + user.nickname} user={user} />)
+            }
+          </div>
+        </div>
+      </ContentContainer>
+    </PageContainer>
+  );
 };
 
-export default Search;
+Searches.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <div className="h-screen flex flex-col">
+      <div className="flex-initial">
+        <Navbar />
+      </div>
+      {page}
+    </div>
+  );
+};
+
+export default Searches;
