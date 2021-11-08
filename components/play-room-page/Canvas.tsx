03e91477b4a4c467edde52f28f7a25174ade2c89
강명구 <mg5566@hanmail.net>
2021-11-08 17:00:37 +0900

    feat: 게임 data 를 적용해서 화면 그리기

--- components/play-room-page/Canvas.tsx
+++ components/play-room-page/Canvas.tsx
@@ -1,20 +1,68 @@
 import {
-  useEffect, useRef,
+  useEffect, useRef, VFC,
 } from 'react';
+import { IGameUpdateData } from '@/typings/db';
 
-const Canvas = () => {
+interface IProps {
+  updateData: IGameUpdateData[] | null;
+  role: string | null;
+  draw: (context: CanvasRenderingContext2D | null | undefined) => void;
+}
+
+const Canvas: VFC<IProps> = ({ updateData, role, draw }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
+    // context?.fillStyle = 'red';
 
+    // clear
+    context?.clearRect(0, 0, context.canvas.width, context.canvas.height);
     // x_pos, y_pos, width, height
-    context?.fillRect(0, 0, 20, 80);
-  }, []);
+    // draw
+    // context?.fillRect(100, 100, 20, 80);
+    draw(context);
+    console.log('updateData 로 그림 그리기', updateData);
+    if (role === 'player1') {
+      console.log('1p');
+      // context?.fillRect(100, 200, 20, 80);
+      if (updateData) {
+        context?.fillRect(
+          updateData[0].x,
+          updateData[0].y,
+          updateData[0].width,
+          updateData[0].height,
+        );
+      }
+    }
+    if (role === 'player2') {
+      console.log('2p');
+      // context?.fillRect(0, 0, 20, 80);
+      if (updateData) {
+        context?.fillRect(
+          updateData[1].x,
+          updateData[1].y,
+          updateData[1].width,
+          updateData[1].height,
+        );
+      }
+    }
+    if (role === 'ball') {
+      console.log('ball');
+      if (updateData) {
+        context?.fillRect(
+          updateData[2].x,
+          updateData[2].y,
+          updateData[2].width,
+          updateData[2].height,
+        );
+      }
+    }
+  }, [draw, role, updateData]);
 
   return (
-    <canvas ref={canvasRef} />
+    <canvas ref={canvasRef} width="1400" height="500" />
   );
 };
 
--- components/play-room-page/GameScreen.tsx
+++ components/play-room-page/GameScreen.tsx
@@ -1,15 +1,21 @@
 import { VFC } from 'react';
+import { IGameScreenData, IGameUpdateData } from '@/typings/db';
+import Canvas from '@/components/play-room-page/Canvas';
 
 interface IProps {
   onClickReady1P: () => void;
   onClickReady2P: () => void;
   isReady1P: boolean;
   isReady2P: boolean;
-  role: string;
+  initData: IGameScreenData | null;
+  updateData: IGameUpdateData[] | null;
+  draw: (context: CanvasRenderingContext2D | null | undefined) => void;
 }
 
 const GameScreen: VFC<IProps> = ({
-  onClickReady1P, onClickReady2P, isReady1P, isReady2P, role,
+  onClickReady1P, onClickReady2P, isReady1P, isReady2P,
+  initData, updateData,
+  draw,
 }) => {
   // 향후 props 를 통해서 이름을 받아온다.
   const player1 = 'mykang';
@@ -17,6 +23,7 @@ const GameScreen: VFC<IProps> = ({
   // 향후 state or websocket 을 통해서 score 를 받아온다.
   const score1 = 0;
   const score2 = 0;
+  console.log('initData', initData);
 
   return (
     <div className="absolute w-full h-full">
@@ -32,21 +39,22 @@ const GameScreen: VFC<IProps> = ({
           {`${player2}`}
         </div>
       </div>
-      <div className="bg-gray-400 h-11/12 justify-between items-center">
+      <div id="fuck" className="w-full bg-gray-400 h-11/12 justify-between items-center">
         {/* game screen */}
+        <Canvas updateData={updateData} role={initData && initData.role} draw={draw} />
         <button
           type="button"
           onClick={onClickReady1P}
-          className={`w-1/5 h-1/5 rounded-full ${isReady1P && 'bg-amber-500'} ${!isReady1P && 'bg-gray-400'}`}
-          disabled={role !== '1P'}
+          className={`w-1/5 h-1/5 rounded-full ${isReady1P && 'bg-amber-500'} ${!isReady1P && 'bg-gray-200'}`}
+          disabled={initData?.role !== 'player1'}
         >
           ready!
         </button>
         <button
           type="button"
           onClick={onClickReady2P}
-          className={`w-1/5 h-1/5 rounded-full ${isReady2P && 'bg-amber-500'} ${!isReady2P && 'bg-gray-400'}`}
-          disabled={role !== '2P'}
+          className={`w-1/5 h-1/5 rounded-full ${isReady2P && 'bg-amber-500'} ${!isReady2P && 'bg-gray-200'}`}
+          disabled={initData?.role !== 'player2'}
         >
           ready!
         </button>
