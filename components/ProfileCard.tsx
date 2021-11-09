b77fd5d814a2d7482fd8c57125443028397ec81f
jiwlee <ddazi9576@google.com>
2021-11-09 16:13:43 +0900

    feat: ProfileCard에 Buttons 추가

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -2,8 +2,10 @@ import { VFC } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import Link from 'next/link';
 import { IGameResultWinRate, IUser } from '@/typings/db';
 import fetcher from '@/utils/fetcher';
+import ProfileCardButtons from './profile-page/ProfileCardButtons';
 
 interface IProps {
   profileUserData: IUser;
@@ -13,11 +15,10 @@ const ProfileCard: VFC<IProps> = ({ profileUserData }) => {
   const router = useRouter();
   const { pathname } = router;
   const { nickname, imagePath } = profileUserData;
+  const { data: userData } = useSWR<IUser>('/api/user/me', fetcher);
   const { data: winData } = useSWR<IGameResultWinRate>(
     pathname === '/play' ? `/api/game/${profileUserData.userId}/win_rate` : null, fetcher,
   );
-
-  console.log(winData);
 
   return (
     <div className="py-10 px-10 rounded-md bg-gray-200">
@@ -32,7 +33,7 @@ const ProfileCard: VFC<IProps> = ({ profileUserData }) => {
       {
         pathname === '/play'
         && (
-        <div className="text-xl py-2 text-center flex flex-col bg-gray-400 mx-16 rounded-md">
+        <div className="mt-5 text-xl py-2 text-center flex flex-col bg-gray-400 mx-16 rounded-md">
           <p>
             승률:
             <span className="text-3xl">{winData?.winRate}</span>
@@ -41,7 +42,23 @@ const ProfileCard: VFC<IProps> = ({ profileUserData }) => {
           <p className="text-xl">{`${winData?.totalPlayCount}전 ${winData?.winCount}승 ${winData?.loseCount}패`}</p>
         </div>
         )
-}
+      }
+      {
+        pathname === '/profile/[name]' && userData?.nickname === profileUserData.nickname
+          ? (
+            <div className="text-center mt-5">
+              <Link href="/edit-profile">
+                <a className="bg-gray-400 px-3 py-1 rounded-full">
+                  Edit Profile
+                </a>
+              </Link>
+            </div>
+          ) : (
+            <div className="text-center mt-5">
+              <ProfileCardButtons profileUser={profileUserData} />
+            </div>
+          )
+      }
     </div>
   );
 };
