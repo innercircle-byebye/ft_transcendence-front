64a72b2be89931097d1391cc913aeddfb8632071
jiwlee <ddazi9576@google.com>
2021-11-09 17:09:01 +0900

    style: HistoryItem에 textColor props 추가

--- components/profile-page/HistoryItem.tsx
+++ components/profile-page/HistoryItem.tsx
@@ -1,7 +1,11 @@
 import { VFC } from 'react';
 
-const HistoryItem: VFC = () => (
-  <div className="bg-amber-50 text-xl text-sky-700 rounded-md px-5 py-2 flex justify-between">
+interface IProps {
+  textColor?: string;
+}
+
+const HistoryItem: VFC<IProps> = ({ textColor = 'text-sky-700' }) => (
+  <div className={`${textColor} bg-amber-50 text-xl rounded-md px-5 py-2 flex justify-between`}>
     <span>2021.11.09</span>
     <span>kat</span>
     <span>승</span>
