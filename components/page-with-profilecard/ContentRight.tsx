1c32fedf478a749255c1d1c2693dec7c726854f3
jiwlee <ddazi9576@google.com>
2021-11-09 17:52:25 +0900

    style: background color props 추가

--- components/page-with-profilecard/ContentRight.tsx
+++ components/page-with-profilecard/ContentRight.tsx
@@ -1,7 +1,15 @@
 import type { FC } from 'react';
 
-const ContentRight: FC = ({ children }) => (
-  <div className="col-span-2 h-full">{children}</div>
+interface IProps {
+  bgColor?: string,
+}
+
+const ContentRight: FC<IProps> = ({ children, bgColor }) => (
+  <div className={`col-span-2 h-full ${bgColor}`}>{children}</div>
 );
+
+ContentRight.defaultProps = {
+  bgColor: 'bg-white',
+};
 
 export default ContentRight;
