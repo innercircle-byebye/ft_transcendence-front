1f6b3b3a9171d05d3b56381722f72b9568a1694d
jiwlee <ddazi9576@google.com>
2021-11-09 18:02:37 +0900

    refactor: HistoryContentContainer 삭제

--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -3,26 +3,38 @@ import React, { ReactElement } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Navbar from '@/components/navigation-bar/Navbar';
 import ProfileCard from '@/components/page-with-profilecard/ProfileCard';
-import HistoryContentContainer from '@/components/history-page/HistoryContentContainer';
 import PageContainer from '@/components/page-with-profilecard/PageContainer';
 import ContentContainer from '@/components/page-with-profilecard/ContentContainer';
 import ContentLeft from '@/components/page-with-profilecard/ContentLeft';
 import ContentRight from '@/components/page-with-profilecard/ContentRight';
+import HistorySelect from '@/components/history-page/HistorySelect';
+import useInput from '@/hooks/useInput';
+import HistoryList from '@/components/profile-page/HistoryList';
 
 const History = ({ userInitialData }
-  : InferGetServerSidePropsType<typeof getServerSideProps>) => (
+  : InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [nickname, onChangeNickname] = useInput('');
+
+  return (
     <PageContainer maxWidth="xl">
       <ContentContainer>
         <ContentLeft>
           <ProfileCard profileUserData={userInitialData} />
         </ContentLeft>
         <ContentRight>
-          <HistoryContentContainer />
+          <div className="mb-4">
+            <h1 className="text-4xl leading-10">History</h1>
+          </div>
+          <div className="p-4 rounded-md bg-gray-300 space-y-5">
+            <HistorySelect nickname={nickname} onChangeNickname={onChangeNickname} />
+            <HistoryList />
+          </div>
         </ContentRight>
       </ContentContainer>
       <ToastContainer />
     </PageContainer>
-);
+  );
+};
 
 History.getLayout = function getLayout(page: ReactElement) {
   return (
