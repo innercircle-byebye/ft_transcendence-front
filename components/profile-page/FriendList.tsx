fe025ebac1e22b5fadd6a307181ae088dcaa6bbc
Sungyong Cho <to.sungyongcho@gmail.com>
2021-11-10 13:58:34 +0900

    탭 버튼 구현

---
+++ components/profile-page/FriendList.tsx
@@ -0,0 +1,53 @@
+import React, { useState, VFC } from 'react';
+import HistoryItem from './HistoryItem';
+
+const FriendList: VFC = () => {
+  const [clickedItem, setClickedItem] = useState('friendList');
+
+  const handleClick = (list: string) => {
+    setClickedItem(list);
+  };
+  return (
+    <div>
+      <div className="flex items-end justify-left">
+        <button
+          type="button"
+          onClick={() => handleClick('friendList')}
+          className={`px-5 py-2 text-white text-xl rounded-t-md ${clickedItem === 'friendList' ? 'bg-sky-300' : 'bg-sky-700'}`}
+        >
+          친구 목록
+        </button>
+        <button
+          type="button"
+          onClick={() => handleClick('friendReceivedList')}
+          className={`px-5 py-2 text-white text-xl rounded-t-md ${clickedItem === 'friendReceivedList' ? 'bg-sky-300' : 'bg-sky-700'}`}
+        >
+          요청받은 친구목록
+        </button>
+        <button
+          type="button"
+          onClick={() => handleClick('friendSentList')}
+          className={`px-5 py-2 text-white text-xl rounded-t-md ${clickedItem === 'friendSentList' ? 'bg-sky-300' : 'bg-sky-700'}`}
+        >
+          요청보낸 친구목록
+        </button>
+        <button
+          type="button"
+          onClick={() => handleClick('friendBlockedList')}
+          className={`px-5 py-2 text-white text-xl rounded-t-md ${clickedItem === 'friendBlockedList' ? 'bg-sky-300' : 'bg-sky-700'}`}
+        >
+          차단 목록
+        </button>
+      </div>
+      <div className="bg-sky-700 p-5 space-y-5 rounded-r-md rounded-bl-md">
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+        <HistoryItem />
+      </div>
+    </div>
+  );
+};
+
+export default FriendList;
--- pages/profile/[name].tsx
+++ pages/profile/[name].tsx
@@ -10,6 +10,7 @@ import ProfileCard from '@/components/ProfileCard';
 import RankItem from '@/components/profile-page/RankItem';
 import WinScore from '@/components/profile-page/WinScore';
 import HistoryList from '@/components/profile-page/HistoryList';
+import FriendList from '@/components/profile-page/FriendList';
 
 const Profile = ({ userInitialData }
   : InferGetServerSidePropsType<typeof getServerSideProps>) => {
@@ -35,6 +36,7 @@ const Profile = ({ userInitialData }
             <RankItem rankInfo={profileUserData.rankInfo} />
             <WinScore userId={profileUserData.userId} />
           </div>
+          <FriendList />
           <HistoryList />
         </div>
       </div>
