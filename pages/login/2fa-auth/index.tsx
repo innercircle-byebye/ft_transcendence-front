3bc3b1ed993dfeb1e9d3e6f69a9e205e1baba9bb
jiwlee <ddazi9576@google.com>
2021-11-12 15:48:26 +0900

    feat: login 2fa auth 페이지 생성

---
+++ pages/login/2fa-auth/index.tsx
@@ -0,0 +1,10 @@
+import ContentContainer from '@/components/create-profile-page/ContentContainer';
+import PageContainer from '@/components/create-profile-page/PageContainer';
+
+const TwoFactorAuth = () => (
+  <PageContainer>
+    <ContentContainer />
+  </PageContainer>
+);
+
+export default TwoFactorAuth;
