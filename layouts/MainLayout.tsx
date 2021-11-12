4dec70b2775773280bc02eb221620f56107d7543
jiwlee <ddazi9576@google.com>
2021-11-12 23:51:17 +0900

    refactor: MainLayout flex 적용

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -3,15 +3,17 @@ import Head from 'next/head';
 import Navbar from '@/components/navigation-bar/Navbar';
 
 const MainLayout: FC = ({ children }) => (
-  <>
+  <div className="h-screen flex flex-col">
     <Head>
       <title>Pong&Chat</title>
       <meta name="description" content="Play pong game and Chat" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Navbar />
-    <main>{children}</main>
-  </>
+    <div className="flex-initial">
+      <Navbar />
+    </div>
+    {children}
+  </div>
 );
 
 export default MainLayout;
