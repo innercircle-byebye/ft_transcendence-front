a70c85cf971f288f9f8b9ad78a08481c1ead436c
jiwlee <ddazi9576@google.com>
2021-11-15 22:38:40 +0900

    feat: Block한 유저 DMList에서 제거

--- components/chat-page/layout/DMList.tsx
+++ components/chat-page/layout/DMList.tsx
@@ -8,6 +8,7 @@ import EachDM from '@/components/chat-page/dm/EachDM';
 const DMList: VFC = () => {
   const { data: dmMembersDatas } = useSWR<IUser[]>('/api/dm/users', fetcher);
   const [dmCollapse, setDMCollapse] = useState(false);
+  const { data: blockMemberData } = useSWR<IUser[]>('/api/block/list', fetcher);
 
   return (
     <div className="border-2 border-sky-700 bg-sky-50 rounded-lg w-full h-auto p-3 space-y-3">
@@ -17,9 +18,16 @@ const DMList: VFC = () => {
       </div>
       <div className="flex flex-col max-h-80 overflow-y-auto">
         {!dmCollapse
-          && dmMembersDatas?.map((member) => (
-            <EachDM key={member.userId + member.nickname} member={member} />
-          ))}
+          && dmMembersDatas?.map((member) => {
+            if (blockMemberData?.map(
+              (blockMember) => blockMember.userId,
+            ).includes(member.userId)) {
+              return null;
+            }
+            return (
+              <EachDM key={member.userId + member.nickname} member={member} />
+            );
+          })}
       </div>
     </div>
   );
