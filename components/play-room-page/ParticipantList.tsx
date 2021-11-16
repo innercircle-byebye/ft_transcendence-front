f63d8e325e751c2df88e109f68cef2f43fb067c1
강명구 <mg5566@hanmail.net>
2021-11-17 01:50:29 +0900

    feat: 강퇴버튼 조건 추가

--- components/play-room-page/ParticipantList.tsx
+++ components/play-room-page/ParticipantList.tsx
@@ -12,18 +12,19 @@ const ParticipantList: VFC<IProps> = ({
   myRole,
   onClickKick,
 }) => (
-  <div className="w-full h-full bg-sky-200">
+  <div className="w-full h-full bg-sky-200 space-y-1">
     관전자 목록입니다.
     {participantData.map((item) => (
       <div
         key={item.key}
-        className="pl-2 flex justify-evenly"
+        className="px-2 flex justify-between"
       >
         {`${item.role} : ${item.nickname}`}
-        {myRole === 'player1' ? (
+        {myRole === 'player1' && item.role !== 'player1' && item.nickname !== '' ? (
           <button
             type="button"
             onClick={() => onClickKick(item.userId)}
+            className="bg-red-400 rounded-md"
           >
             강퇴
           </button>
