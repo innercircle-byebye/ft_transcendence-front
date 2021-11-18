4d9e73ab663223dcbc76511e6818babcef876273
jiwlee <ddazi9576@google.com>
2021-11-19 04:36:27 +0900

    refactor: onScroll 필요없어서 삭제

--- components/play-room-page/GameChatList.tsx
+++ components/play-room-page/GameChatList.tsx
@@ -1,5 +1,5 @@
 import {
-  forwardRef, MutableRefObject, useCallback,
+  forwardRef,
 } from 'react';
 import Scrollbars from 'react-custom-scrollbars-2';
 import { IGameChat } from '@/typings/db';
@@ -10,32 +10,20 @@ interface IProps {
 
 const GameChatList = forwardRef<Scrollbars, IProps>((
   { gameChatList }, scrollRef,
-) => {
-  const onScroll = useCallback(
-    (values) => {
-      const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
-      if (current) {
-        current.scrollTop(values.scrollHeight);
-      }
-    },
-    [scrollRef],
-  );
-
-  return (
-    <div className="w-full flex-1 bg-sky-200">
-      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
-        {gameChatList.map((item: IGameChat) => (
-          <div
-            key={item.index}
-            className="pl-2 font-medium"
-          >
-            {`${item.nickname} : ${item.content}`}
-          </div>
-        ))}
-      </Scrollbars>
-    </div>
-  );
-});
+) => (
+  <div className="w-full flex-1 bg-sky-200">
+    <Scrollbars autoHide ref={scrollRef}>
+      {gameChatList.map((item: IGameChat) => (
+        <div
+          key={item.index}
+          className="pl-2 font-medium"
+        >
+          {`${item.nickname} : ${item.content}`}
+        </div>
+      ))}
+    </Scrollbars>
+  </div>
+));
 
 GameChatList.displayName = 'GameChatList';
 
